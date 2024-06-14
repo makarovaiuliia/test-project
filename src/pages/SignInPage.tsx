7ecc932354610244786c574dfa0a99a8eebbbe8a
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-06-14 18:22:31 +0200

    feat: add signInPage

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,9 @@
+import SignInForm from '@/components/SignInForm';
+
+export default function SignInPage() {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <SignInForm />
+        </div>
+    );
+}
